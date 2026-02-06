8570b16d047bd3e814c44df062a76b61f2d4d255
John Schop <jschop@vmi-group.com>
2026-02-06 13:47:08 +0100

    Run timelapse immediately at startup instead of waiting for first interval

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+from datetime import datetime
 from motion_detection import detector, scheduler, sync_to_gdrive
 from webserver import app
 from settings import WEBSERVER_HOST
@@ -8,9 +9,11 @@ if __name__ == '__main__':
     # Start motion detection
     detector.start()
     scheduler_interval = get_setting('SCHEDULER_INTERVAL_MINUTES', 30)
-    scheduler.add_job(func=lambda: detector.capture_timelapse(), trigger="interval", minutes=scheduler_interval)
+    # Run timelapse immediately at startup, then repeat every interval
+    scheduler.add_job(func=lambda: detector.capture_timelapse(), trigger="interval", minutes=scheduler_interval, next_run_time=datetime.now())
     scheduler.add_job(func=sync_to_gdrive, trigger="interval", hours=1)
     scheduler.start()
+    print(f"Scheduler started - timelapse will run immediately and then every {scheduler_interval} minutes")
     try:
         # Run webserver
         port = get_setting('WEBSERVER_PORT', 5000)
